9e9fe50a4094d003b03d6d0f0055f276b0e85ccb
mdselimme <mdselimdev@gmail.com>
2025-09-25 06:47:31 +0600

    add: next auth logout implement

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,18 @@
-const DashboardHomePage = () => {
+import { authOptions } from "@/helpers/authOptions";
+import { getServerSession } from "next-auth";
+
+const DashboardHomePage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
   return (
-    <div>
-      <h1>Dashboard Home Page</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-6 w-full">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome, {session?.user?.name}
+      </h1>
+      <p className="text-lg text-gray-600 italic text-center">
+        {session?.user?.email}
+      </p>
     </div>
   );
 };
